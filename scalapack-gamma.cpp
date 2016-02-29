d8b9b4db6edaa40ed9587095a129af8c36d59847
Hadi Montakhabi <hadi.montakhabi@gmail.com>
2016-02-29 14:47:40 -0600

    put rand back instead of 1

--- scalapack-gamma.cpp
+++ scalapack-gamma.cpp
@@ -72,7 +72,7 @@ int main(int argc, char **argv)
 
     for (int r = 0; r < N; ++r) {
       for (int c = 0; c < M; ++c) {
-	*(X_global+N*c + r) = 1.;//(double) rand()/RAND_MAX;
+	*(X_global+N*c + r) = (double) rand()/RAND_MAX;
       }
     }
 
